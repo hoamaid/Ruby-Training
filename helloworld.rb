9292d03629c2400c62b88ab6d2892f4f9ac85230
Hoa Mai <hoa@thelevelup.com>
2017-02-02 15:08:07 -0500

    This is my change for today

--- helloworld.rb
+++ helloworld.rb
@@ -1,5 +1,19 @@
-myName = "Hoa Mai"
-print "Hello "
-print myName
-puts "!!!"
+#This is my very first lesson in Ruby!
 
+print "What is your name?"
+your_name = gets.chomp
+your_name.capitalize!
+puts "Hello #{your_name}!!!"
+
+
+=begin 
+
+print "What is your name?"
+answer = gets.chomp
+answer2 = answer.capitalize!
+
+capitalized = "HoaMai Print this"
+print capitalized
+
+
+=end

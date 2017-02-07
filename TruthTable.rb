fb64312657420f3dc681f36623a20de06c22ca82
Hoa Mai <hoa@thelevelup.com>
2017-02-07 16:01:02 -0500

    Conditions and Truthtable

---
+++ TruthTable.rb
@@ -0,0 +1,11 @@
+puts "T && T is #{true && true}"
+puts "T && F is #{true && false}"
+puts "F && T is #{false && true}"
+puts "F && F is #{false && false}"
+
+
+
+puts "T || T is #{true || true}"
+puts "T || F is #{true || false}"
+puts "F || T is #{false || true}"
+puts "F || F is #{false || false}"
--- controlflow.rb
+++ controlflow.rb
@@ -10,4 +10,21 @@ else
 	print "b is greater than a"
 end
 
-puts "done!"
+x = c < a
+unless x
+	puts "c > a"
+end
+
+puts "b > c" unless b < c
+
+puts " Done!"
+
+
+# test_1 should be true
+test_1 = 3 > 1 && 2 < 4
+
+# test_2 = should be true
+test_2 = 5 < 6 || 7 > 1
+
+# test_3 = should be false
+test_3 = !(2 > 1) && 5 < 8
